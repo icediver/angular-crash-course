487e36b9e32f9e787025492de675c2cea18f26f8
icediver <icedivet@live.ru>
2023-09-25 17:33:59 +0300

    finish

---
+++ src/app/data/product.ts
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    id: 1,
+    title: 'Fjallraven = Foldsack No. 1 Backpack, Fits 15 Laptops',
+    price: 109.95,
+    description:
+      'Your perfect pack for everday use and walks in the forest. Stach your laptop(up to 15 inches) in the padded sleeve, your everyday',
+    category: "men's clothing",
+    image: 'http://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    rating: {
+      rate: 3.9,
+      count: 120,
+    },
+  },
+  {
+    id: 2,
+    title: 'Mens Casual Premium Slim Fit T-shirts',
+    price: 22.3,
+    description:
+      'Slim-fitting style, contrast raglan long sleeve, thre-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stiched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
+    category: "men's clothing",
+    image:
+      'http://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+    rating: {
+      rate: 4.1,
+      count: 259,
+    },
+  },
+];
